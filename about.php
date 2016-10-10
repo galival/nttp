83987a36ddf1e62be56657a2042c49e5d6565fef
galival <alina.grigorovitch@gmail.com>
2016-10-10 17:27:45 -0400

    added blog.php and about.php pages. both still need content.

---
+++ about.php
@@ -0,0 +1,9 @@
+<?php
+require 'header.php';
+
+echo '<div class="body-block" id="center-block">
+	<div class="block-header"><h1>This is the portfolio of Alina Grigorovitch.</h1></div>
+	</div>';
+
+include 'footer.php';
+?>
---
+++ blog.php
@@ -0,0 +1,7 @@
+<?php
+require 'header.php';
+echo '<div class="body-block" id="center-block">
+	<div class="block-header"><h1>Blog coming soon.</h1></div>
+	</div>';
+include 'footer.php';
+?>
